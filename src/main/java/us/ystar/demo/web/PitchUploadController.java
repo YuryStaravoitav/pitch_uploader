134e2755096ae51aa7486d771766fbaa4ec2a9b0
Yury Starav <yury.starav@gmail.com>
2021-06-06 22:22:56 -0700

    Implement multi threading converting pdf pages into images

--- src/main/java/us/ystar/demo/web/PitchUploadController.java
+++ src/main/java/us/ystar/demo/web/PitchUploadController.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import us.ystar.demo.PitchPdfUtils;
 import us.ystar.demo.services.FileService;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,6 +21,9 @@ public class PitchUploadController extends BasePitchController {
 
     @Autowired
     private FileService fileService;
+
+    @Autowired
+    private PitchPdfUtils pitchPdfUtils;
 
     private static final List<String> contentTypes = Arrays.asList("application/x-pdf", "application/pdf");
     //, "application/vnd.openxmlformats-officedocument.presentationml.presentation", "application/vnd.ms-powerpoint");
@@ -29,16 +34,18 @@ public class PitchUploadController extends BasePitchController {
     }
 
     @PostMapping("/uploadFile")
-    public String uploadFile(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes, Model model) {
+    public String uploadFile(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes, Model model) throws IOException, InterruptedException {
         String fileContentType = file.getContentType();
         if (!contentTypes.contains(fileContentType)) {
             model.addAttribute("message", String.format("We don't support %s file type", fileContentType));
             return "index";
         }
         filePath = fileService.uploadFile(file);
+        List<String> imageFileNames = pitchPdfUtils.convertPdfToImage(filePath, file.getOriginalFilename());
         redirectAttributes.addFlashAttribute("message", String.format("You successfully uploaded the %s file", file.getOriginalFilename()));
         redirectAttributes.addFlashAttribute("filePath", filePath);
         redirectAttributes.addFlashAttribute("fileName", file.getOriginalFilename());
+        redirectAttributes.addFlashAttribute("images", imageFileNames);
         return "redirect:/view/slides";
     }
 }
--- src/main/java/us/ystar/demo/web/PitchViewController.java
+++ src/main/java/us/ystar/demo/web/PitchViewController.java
@@ -9,26 +9,29 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import us.ystar.demo.PitchPdfUtils;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class PitchViewController {
 
-    @Autowired
-    private PitchPdfUtils pitchPdfUtils;
 
     @GetMapping("/view/slides")
     public String showViewPage(
             Model model,
             @ModelAttribute("filePath") final String filePath,
-            @ModelAttribute("fileName") final String fileName) throws IOException {
+            @ModelAttribute("fileName") final String fileName,
+            @ModelAttribute("images") final List<String> imageFileNames) throws IOException {
         if (filePath == null || filePath.isEmpty()) {
             return "redirect:/";
         }
+        if (imageFileNames == null) {
+            System.out.println("imagesFileNames");
+        }
         model.addAttribute("filePath", filePath);
-        List<String> imageFileNames = pitchPdfUtils.convertPdfToImage(filePath, fileName);
         model.addAttribute("images", imageFileNames);
         model.addAttribute("filePath", filePath);
+
         return "view/slides";
     }
 }
